f91f61d69861645fa4d403a35af6ce597515943f
itsprk <prk.shine@gmail.com>
2019-10-23 22:19:04 +0530

    Updates

---
+++ precache-manifest.e161fae1cd9ca1669e532f2edc76ed73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e2d34a22b91b72f9542effa0a23acd6",
+    "url": "/React-Router/index.html"
+  },
+  {
+    "revision": "6a4f56456d028d631853",
+    "url": "/React-Router/static/css/main.1bac61af.chunk.css"
+  },
+  {
+    "revision": "4e5875e6c8d2f691f07c",
+    "url": "/React-Router/static/js/2.1b73b72b.chunk.js"
+  },
+  {
+    "revision": "6a4f56456d028d631853",
+    "url": "/React-Router/static/js/main.8ea5726c.chunk.js"
+  },
+  {
+    "revision": "c146d43579c27ee7526c",
+    "url": "/React-Router/static/js/runtime-main.33830dff.js"
+  }
+]);
